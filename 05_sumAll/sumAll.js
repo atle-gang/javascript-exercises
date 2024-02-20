fcfaa5b64b723ffc07387f697840486c3f9188bc
Atlegang Sethono <asethono023@student.wethinkcode.co.za>
2024-02-20 13:24:45 +0200

    Complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+    let sum = 0;
+    if (x < 0 || y < 0) {
+        return "ERROR";
+    } else if (typeof x !== 'number' || typeof y !== 'number' || typeof y === 'object') {
+        return "ERROR";
+        
+    } else if(x > y) {
+        for (let i = x; i >= y; i--) {
+            sum += i;   
+        }
+    } else {
+        for (let i = x; i <= y; i++) {
+            sum += i;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+console.log(sumAll(123, 1))
