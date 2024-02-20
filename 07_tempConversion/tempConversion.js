4eceda44d9b38607f61fe4164dc02f5a14530de8
Atlegang Sethono <asethono023@student.wethinkcode.co.za>
2024-02-20 13:54:24 +0200

    Complete 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+
+
+const convertToCelsius = function(temp) {
+  let celsius;
+  // °C = (°F - 32) × 5/9
+  celsius = (temp - 32) * 5 / 9
+  return Math.round(celsius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let fahrenheit;
+  // F = (9/5 × °C) + 32
+  fahrenheit = (9 / 5 * temp) + 32;
+  return Math.round(fahrenheit * 10) / 10
 };
 
 // Do not edit below this line
@@ -9,3 +19,5 @@ module.exports = {
   convertToCelsius,
   convertToFahrenheit
 };
+
+// console.log(convertToFahrenheit(0))
