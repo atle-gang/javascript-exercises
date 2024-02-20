518b5037dd2e3a73b2922e54916f933e112b38d6
Atlegang Sethono <asethono023@student.wethinkcode.co.za>
2024-02-20 11:55:55 +0200

    Complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+    newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (!args.includes(arr[i])) {
+            newArr.push(arr[i]);
+        }
+    }
+    return newArr;
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+// console.log(removeFromArray([1, 2, 3, 4], 3));
