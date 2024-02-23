06326108142d7e821d3524f6a7e30cee206a65a3
Atlegang Sethono <atlegangcode@gmail.com>
2024-02-23 10:17:55 +0200

    Complete 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,50 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	sumAll = 0;
+  if (arr.length === 0) {
+    return sumAll;
+  } else if (arr.length === 1) {
+    sumAll += arr[0]
+  } else {
+    for (i = 0; i < arr.length; i++) {
+      sumAll += arr[i];
+    }
+  }
+  return sumAll;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  sumAll = 1;
+  if (arr.length === 0) {
+    return sumAll;
+  } else if (arr.length === 1) {
+    sumAll *= arr[0]
+  } else {
+    for (i = 0; i < arr.length; i++) {
+      sumAll *= arr[i];
+    }
+  }
+  return sumAll;
 };
 
-const power = function() {
-	
+
+const power = function(a, b) {
+	return a ** b
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  if (n === 0 || n === 1) {
+    return 1;
+  } else {
+    return n * factorial(n - 1)
+  }
 };
 
 // Do not edit below this line
@@ -31,3 +56,5 @@ module.exports = {
   power,
   factorial
 };
+
+console.log(power(4, 3))
