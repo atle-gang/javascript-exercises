5692c1184e358ea450308533ce22899d9e20d28d
Atlegang Sethono <atlegangcode@gmail.com>
2024-02-23 10:59:17 +0200

    Complete 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n < 0 || isNaN(parseInt(n))) {
+        return "OOPS";
+    } else {
+        n = parseInt(n);
+        if ( n === 0) {
+            return 0;
+        } else if ( n === 1) {
+            return 1;
+        } else if (n === 2) {
+            return 1;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
+console.log(fibonacci("1"));
